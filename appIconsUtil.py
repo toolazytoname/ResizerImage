ce5338c4cdf6fbfa895fb18fe63cf2f8054de9ab
toolazytoname <shuitaiyang747@qq.com>
2014-09-01 17:49:02 +0800

    add files

---
+++ appIconsUtil.py
@@ -0,0 +1,19 @@
+import os
+import Image
+import string
+
+inFile = 'OrangeDragon.png'
+sizeArray = ['57','29','72','50','29','32','40','1024']
+
+for outSize in sizeArray: 
+	fileName = os.path.splitext(inFile)[0]
+	im = Image.open(inFile)
+
+	outFile =  fileName + '_' + outSize + 'x' + outSize + '.png'
+	outImage = im.resize((string.atoi(outSize),string.atoi(outSize)),Image.ANTIALIAS)
+	outImage.save(outFile)
+
+	outFile2x = fileName + '_' + outSize + 'x' + outSize + '@2x' +'.png'
+	outImage2x = im.resize((string.atoi(outSize) * 2,string.atoi(outSize) * 2),Image.ANTIALIAS)
+	outImage2x.save(outFile2x)
+
